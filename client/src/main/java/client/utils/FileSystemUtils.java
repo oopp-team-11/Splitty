373d8f6b8628f9cc48b11e62d6f0a831e1882298
Marios Kailis <m.kailis@student.tudelft.nl>
2024-02-21 11:34:50 +0100

    Feature that stores client's data into json config file on client's disk

---
+++ client/src/main/java/client/utils/FileSystemUtils.java
@@ -0,0 +1,23 @@
+package client.utils;
+
+import javax.json.JsonObject;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileSystemUtils {
+    private static final String CLIENT_JSON_PATH = "client.json";
+
+
+    //Method that stores the client data in a json file
+    public void saveJsonClient(JsonObject json) {
+        try (FileWriter file = new FileWriter(CLIENT_JSON_PATH)) {
+
+            file.write(json.toString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
