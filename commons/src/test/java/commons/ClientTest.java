3d2b82641ffaa12d1e8ea3338a2a3de7a70f17e6
Adam Szymaniak <a.p.szymaniak@student.tudelft.nl>
2024-02-22 18:45:48 +0100

    Added tests for getters and setters of Client

--- commons/src/test/java/commons/ClientTest.java
+++ commons/src/test/java/commons/ClientTest.java
@@ -70,4 +70,69 @@ class ClientTest {
         assertTrue(clientToString.contains("iban"));
         assertTrue(clientToString.contains("bic"));
     }
+
+    @Test
+    void getId() {
+        assertTrue(client.getId() >= 0);
+    }
+
+    @Test
+    void getInvitationCode() {
+        assertEquals("ABC-123-456", client.getInvitationCode());
+    }
+
+    @Test
+    void getFirstName() {
+        assertEquals("John", client.getFirstName());
+    }
+
+    @Test
+    void getLastName() {
+        assertEquals("Doe", client.getLastName());
+    }
+
+    @Test
+    void getEmail() {
+        assertEquals("j.doe@domain.com", client.getEmail());
+    }
+
+    @Test
+    void getIban() {
+        assertEquals("NL91 ABNA 0417 1643 00", client.getIban());
+    }
+
+    @Test
+    void getBic() {
+        assertEquals("ABNANL2A123", client.getBic());
+    }
+
+    @Test
+    void setFirstName() {
+        client.setFirstName("Joe");
+        assertEquals("Joe", client.getFirstName());
+    }
+
+    @Test
+    void setLastName() {
+        client.setLastName("Average");
+        assertEquals("Average", client.getLastName());
+    }
+
+    @Test
+    void setEmail() {
+        client.setEmail("j.average@gmail.com");
+        assertEquals("j.average@gmail.com", client.getEmail());
+    }
+
+    @Test
+    void setIban() {
+        client.setIban("NL91 ABNA 1234 5678 90");
+        assertEquals("NL91 ABNA 1234 5678 90", client.getIban());
+    }
+
+    @Test
+    void setBic() {
+        client.setBic("ABNANL2A567");
+        assertEquals("ABNANL2A567", client.getBic());
+    }
 }
