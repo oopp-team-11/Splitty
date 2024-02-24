7e6f0d085d5e2cda5a11641bc9754ef6ccf24752
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-02-24 09:43:05 +0100

    Expanded Event.java file

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -1,4 +1,103 @@
 package commons;
 
+import jakarta.persistence.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.List;
+import java.util.Objects;
+
+import java.time.LocalDate;
+
+@Entity
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+
+    private LocalDate creationDate;
+
+    private LocalDate lastActivity;
+
+    @OneToMany
+    private List<Participant> participants;
+
+    @OneToMany
+    private List<Participant> expenses;
+
+    public Event(long id, String title, LocalDate creationDate, LocalDate lastActivity,
+                 List<Participant> participants, List<Participant> expenses) {
+        this.id = id;
+        this.title = title;
+        this.creationDate = creationDate;
+        this.lastActivity = lastActivity;
+        this.participants = participants;
+        this.expenses = expenses;
+    }
+
+    public Event() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Participant> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+    public List<Participant> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Participant> expenses) {
+        this.expenses = expenses;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDate getLastActivity() {
+        return lastActivity;
+    }
+
+    public void setLastActivity(LocalDate lastActivity) {
+        this.lastActivity = lastActivity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Event event)) return false;
+
+        return new EqualsBuilder().append(id, event.id).append(title, event.title)
+                .append(creationDate, event.creationDate).append(lastActivity, event.lastActivity)
+                .append(participants, event.participants).append(expenses, event.expenses).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(id).append(title)
+                .append(creationDate).append(lastActivity).append(participants).append(expenses).toHashCode();
+    }
 }
