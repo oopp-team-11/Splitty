cf968efcb93e6bcd2fb098c32e4dd9c961d3f7e1
Marios Kailis <m.kailis@student.tudelft.nl>
2024-03-02 15:53:46 +0100

    Implement client's utils methods and tests

--- client/src/test/java/client/utils/ServerUtilsTest.java
+++ client/src/test/java/client/utils/ServerUtilsTest.java
@@ -1,14 +1,22 @@
 package client.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import commons.Event;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 class ServerUtilsTest {
@@ -92,6 +100,80 @@ class ServerUtilsTest {
 
         assertEquals(responseCode, jsonObject.getInt("invitationCode"));
         assertEquals(200, wireMockServer.getAllServeEvents().get(1).getResponse().getStatus());
+        wireMockServer.stop();
+    }
+
+    @Test
+    void getRecentEventsWireMock() throws IOException, InterruptedException {
+        WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration
+            .options()
+            .port(9092));
+        wireMockServer.start();
+
+        ServerUtils serverUtils = new ServerUtils();
+        List<Long> codes = new ArrayList<>();
+        String randomFileName = UUID.randomUUID().toString() + ".json";
+
+        codes.add(54321L);
+        codes.add(12345L);
+
+        JsonObject json = Json.createObjectBuilder()
+                .add("invitationCodes", Json.createArrayBuilder(codes))
+                .build();
+
+        if(new File(randomFileName).exists()) {
+            new File(randomFileName).delete();
+        }
+
+        FileWriter file = new FileWriter(randomFileName);
+        file.write(json.toString());
+        file.flush();
+        file.close();
+
+        URI uri = URI.create("/events"
+        +"?query=titles&invitationCodes=" + codes.toString()
+                .replace("[", "").
+                replace("]", "").
+                replace(" ", ""));
+
+        String jsonEventsList = "{\n" +
+                "    \"events\": [\n" +
+                "        {\n" +
+                "            \"invitationCode\": 12345,\n" +
+                "            \"eventName\": \"testEvent1\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"invitationCode\": 54321,\n" +
+                "            \"eventName\": \"testEvent2\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+        JSONObject jsonObject = new JSONObject(jsonEventsList);
+        JSONArray jsonArray = new JSONArray(jsonObject
+                .getJSONArray("events").toString());
+
+        List<Event> events = new ArrayList<>();
+        for(Object object: jsonArray) {
+            events.add(new Event(
+                    Long.parseLong(((JSONObject) object)
+                            .get("invitationCode").toString()),
+                    ((JSONObject) object).get("eventName").toString(),
+                    null,
+                    null,
+                    null)
+            );
+        }
+
+        wireMockServer.stubFor(get(urlEqualTo(uri.toString()))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(jsonEventsList)));
+
+        assertEquals(serverUtils.getRecentEvents("http://localhost:9092",
+                        randomFileName), events);
+
+        new File(randomFileName).delete();
         wireMockServer.stop();
     }
 }
