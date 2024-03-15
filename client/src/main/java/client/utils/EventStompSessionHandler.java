f659aebb251817907dea4f04dac2bca94db6ac7c
Adam Szymaniak <a.p.szymaniak@student.tudelft.nl>
2024-03-15 13:48:22 +0100

    Added the send methods to WebSocket handler

--- client/src/main/java/client/utils/EventStompSessionHandler.java
+++ client/src/main/java/client/utils/EventStompSessionHandler.java
@@ -13,17 +13,21 @@ import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
  */
 public class EventStompSessionHandler extends StompSessionHandlerAdapter {
     private final String invitationCode;
+    private StompSession session;
 
     /**
      * Custom constructor for EventStompSessionHandler
+     *
      * @param invitationCode Specifies the event to which the client should listen to
      */
     public EventStompSessionHandler(String invitationCode) {
         this.invitationCode = invitationCode;
     }
+
     @Override
     public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-        session.subscribe("/event/" + invitationCode, this);
+        this.session = session;
+        session.subscribe(invitationCode, this);
     }
 
     @Override
@@ -45,5 +49,47 @@ public class EventStompSessionHandler extends StompSessionHandlerAdapter {
             }
             case null, default -> System.out.println("Model type not specified in the message headers");
         }
+    }
+
+    /**
+     * Sends a message to the server with a Participant update
+     *
+     * @param participant an updated Participant object
+     * @param methodType  supports {"create", "update", "delete"}
+     */
+    public void sendParticipant(Participant participant, String methodType) {
+        StompHeaders headers = new StompHeaders();
+        headers.add("model", "Participant");
+        headers.add("method", methodType);
+        headers.setDestination(invitationCode);
+        session.send(headers, participant);
+    }
+
+    /**
+     * Sends a message to the server with an Event update
+     *
+     * @param event      an updated Event object
+     * @param methodType supports {"create", "update", "delete"}
+     */
+    public void sendEvent(Event event, String methodType) {
+        StompHeaders headers = new StompHeaders();
+        headers.add("model", "Event");
+        headers.add("method", methodType);
+        headers.setDestination(invitationCode);
+        session.send(headers, event);
+    }
+
+    /**
+     * Sends a message to the server with an Expense update
+     *
+     * @param expense    an updated Expense object
+     * @param methodType supports {"create", "update", "delete"}
+     */
+    public void sendExpense(Expense expense, String methodType) {
+        StompHeaders headers = new StompHeaders();
+        headers.add("model", "Expense");
+        headers.add("method", methodType);
+        headers.setDestination(invitationCode);
+        session.send(headers, expense);
     }
 }
