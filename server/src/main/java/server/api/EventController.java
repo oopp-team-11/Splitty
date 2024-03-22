3ac50cae774abd58018e46a31cb66c00e9b31c47
Jakub Pietrzak <jkpietrzak@student.tudelft.nl>
2024-03-22 19:30:27 +0100

    Merged WebSocketController into EventController

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -3,12 +3,19 @@ package server.api;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import commons.Participant;
+import commons.StatusEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import commons.Event;
 import server.database.EventRepository;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -18,17 +25,21 @@ import commons.Views;
  * Server-side EventController for the Event entity.
  * Handles the CRUD operations under all /events endpoints.
  */
-@RestController
-@RequestMapping("/events")
+@Controller
 public class EventController {
     private final EventRepository repo;
+
+    @Autowired
+    private SimpMessagingTemplate template;
 
     /**
      * Constructor for the EventController.
      * Constructed automatically by Spring Boot.
+     * @param template SimpMessagingTemplate
      * @param repo The EventRepository provided automatically by JPA
      */
-    public EventController(EventRepository repo) {
+    public EventController(SimpMessagingTemplate template, EventRepository repo) {
+        this.template = template;
         this.repo = repo;
     }
 
@@ -39,7 +50,7 @@ public class EventController {
      * Returns a 400 Bad Request status code when no invitationCode was provided.
      * Returns a 404 Not Found status code when no Event was found with the provided invitationCode.
      */
-    @GetMapping (path = {"/{invitationCode}", "/{invitationCode}/"})
+    @GetMapping (path = {"events/{invitationCode}", "events/{invitationCode}/"})
     public ResponseEntity<Event> getEventByInvitationCode(@PathVariable("invitationCode") UUID invitationCode) {
         if (invitationCode == null) {
             return ResponseEntity.badRequest().build();
@@ -65,7 +76,7 @@ public class EventController {
      * @return Returns a 200 OK status code and the invitationCode when the Event was successfully saved.
      * Returns a 400 Bad Request status code when no title was provided.
      */
-    @PostMapping (path = {"", "/"})
+    @PostMapping (path = {"events", "events/"})
     public ResponseEntity<Event> createEventWithTitle(@RequestBody String title) {
         if (isNullOrEmpty(title)) {
             return ResponseEntity.badRequest().build();
@@ -87,7 +98,7 @@ public class EventController {
      * Returns a 400 Bad Request status code when no codes were provided.
      */
     @JsonView(Views.UpdateInvitationsCodes.class)
-    @GetMapping(path = {"", "/"})
+    @GetMapping(path = {"events", "events/"})
     public ResponseEntity<List<Event>> updateRecentlyAccessedEvents(@RequestParam("query") String query,
                                                                     @RequestParam("invitationCodes") UUID[] codes) {
         if (codes == null || !"titles".equals(query)) {
@@ -112,7 +123,7 @@ public class EventController {
      * Returns a 400 Bad Request status code when no invitationCode was provided.
      * Returns a 404 Not Found status code when no Event was found with the provided invitationCode.
      */
-    @GetMapping (path = {"/{invitationCode}/participants"})
+    @GetMapping (path = {"events/{invitationCode}/participants"})
     public ResponseEntity<List<Participant>> getParticipantsByInvitationCode(
             @PathVariable("invitationCode") UUID invitationCode)
     {
@@ -124,6 +135,85 @@ public class EventController {
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok(repo.getReferenceById(invitationCode).getParticipants());
+    }
+
+    /**
+     * Handles update websocket endpoint for event
+     * @param principal connection data about user
+     * @param payload content of a websocket message
+     */
+    @MessageMapping("/event:update")
+    public void updateEvent(Principal principal, @Payload Object payload)
+    {
+        if(payload.getClass() != Event.class) {
+            template.convertAndSendToUser(principal.getName(), "/queue/reply",
+                    StatusEntity.badRequest("Payload should be an Event", true));
+            return;
+        }
+
+        Event receivedEvent = (Event) payload;
+
+        if (isNullOrEmpty(receivedEvent.getTitle())) {
+            template.convertAndSendToUser(principal.getName(),"/queue/reply",
+                    StatusEntity.badRequest("Title should not be empty"));
+            return;
+        }
+
+        if(!repo.existsById(receivedEvent.getId()))
+        {
+            template.convertAndSendToUser(principal.getName(),"/queue/reply",
+                    StatusEntity.notFound("Event not found", true));
+            return;
+        }
+
+        Event event = repo.getReferenceById(receivedEvent.getId());
+        event.setTitle(receivedEvent.getTitle());
+        event.setCreationDate(receivedEvent.getCreationDate());
+        event.setLastActivity(receivedEvent.getLastActivity());
+        repo.save(event);
+
+        template.convertAndSend("/topic/"+receivedEvent.getId(), event);
+        template.convertAndSendToUser(principal.getName(), "queue/reply",
+                StatusEntity.ok("event:update " + event.getId()));
+    }
+
+    /**
+     * Handles delete websocket endpoint for event
+     * @param principal connection data about user
+     * @param payload content of a websocket message
+     */
+    @MessageMapping("/event:delete")
+//    @SendTo("/topic/{invitationCode}")
+    public void deleteEvent(Principal principal, @Payload Object payload)
+    {
+        if(payload.getClass() != Event.class) {
+            template.convertAndSendToUser(principal.getName(),"/queue/reply",
+                    StatusEntity.badRequest("Payload should be an Event", true));
+            return;
+        }
+
+        /*TODO: Implement admin passcode verification*/
+//        if()
+//        {
+//            template.convertAndSendToUser(principal.getName(),"/queue/reply",
+//                    new ErrorMessage(new IllegalAccessException()));
+//            return;
+//        }
+
+        Event receivedEvent = (Event) payload;
+        if(!repo.existsById(receivedEvent.getId()))
+        {
+            template.convertAndSendToUser(principal.getName(),"/queue/reply",
+                    StatusEntity.notFound("Event not found", true));
+            return;
+        }
+
+        Event event = repo.getReferenceById(receivedEvent.getId());
+        repo.delete(event);
+
+        template.convertAndSend("/topic/"+receivedEvent.getId(), event);
+        template.convertAndSendToUser(principal.getName(), "queue/reply",
+                StatusEntity.ok("event:delete " + event.getId()));
     }
 
 }
--- server/src/test/java/server/api/EventControllerTest.java
+++ server/src/test/java/server/api/EventControllerTest.java
@@ -1,24 +1,32 @@
 package server.api;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.springframework.http.HttpStatus.*;
 
 
 import commons.Event;
 import commons.Participant;
+import commons.StatusEntity;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
 
 
 public class EventControllerTest {
-    private TestEventRepository repo;
+    private TestEventRepository eventRepo;
+    private SimpMessagingTemplate messagingTemplate;
     private EventController sut;
+    private Principal principal;
 
     private static void setId(Event toSet, UUID newId) throws IllegalAccessException {
         FieldUtils.writeField(toSet, "id", newId, true);
@@ -26,9 +34,11 @@ public class EventControllerTest {
 
 
     @BeforeEach
-    public void setup() {
-        repo = new TestEventRepository();
-        sut = new EventController(repo);
+    void setUp() {
+        messagingTemplate = mock(SimpMessagingTemplate.class);
+        eventRepo = new TestEventRepository();
+        sut = new EventController(messagingTemplate, eventRepo);
+        principal = mock(Principal.class);
     }
 
     // GET: ?query=title&invitationCodes={}
@@ -55,8 +65,8 @@ public class EventControllerTest {
             setId(event1, id1);
             setId(event2, id2);
         } catch (IllegalAccessException ignored) {}
-        repo.save(event1);
-        repo.save(event2);
+        eventRepo.save(event1);
+        eventRepo.save(event2);
         UUID[] recentEvents = new UUID[2];
         recentEvents[0] = id1;
         recentEvents[1] = id2;
@@ -84,7 +94,7 @@ public class EventControllerTest {
     @Test
     public void checkThatEventIsSaved() {
         sut.createEventWithTitle("Trap");
-        assertEquals("Trap", repo.events.getFirst().getTitle());
+        assertEquals("Trap", eventRepo.events.getFirst().getTitle());
     }
 
     @Test
@@ -108,7 +118,7 @@ public class EventControllerTest {
         try {
             setId(toSave, UUID.randomUUID());
         } catch (IllegalAccessException ignored) {}
-        repo.save(toSave);
+        eventRepo.save(toSave);
         UUID invitationCode = toSave.getId();
         var actual = sut.getEventByInvitationCode(invitationCode);
         assertEquals(OK, actual.getStatusCode());
@@ -121,7 +131,7 @@ public class EventControllerTest {
             setId(toSave, UUID.randomUUID());
         } catch (IllegalAccessException ignored) {}
         UUID invitationCode = toSave.getId();
-        repo.save(toSave);
+        eventRepo.save(toSave);
         var actual = sut.getEventByInvitationCode(invitationCode);
         assertEquals("Trap", actual.getBody().getTitle());
     }
@@ -132,7 +142,7 @@ public class EventControllerTest {
         try {
             setId(toSave, UUID.randomUUID());
         } catch (IllegalAccessException ignored) {}
-        repo.save(toSave);
+        eventRepo.save(toSave);
         var actual = sut.getEventByInvitationCode(UUID.randomUUID());
         assertEquals(BAD_REQUEST, actual.getStatusCode());
     }
@@ -142,7 +152,7 @@ public class EventControllerTest {
         try {
             setId(toSave, UUID.randomUUID());
         } catch (IllegalAccessException ignored) {}
-        repo.save(toSave);
+        eventRepo.save(toSave);
 
         var actual = sut.getParticipantsByInvitationCode(UUID.randomUUID());
         assertEquals(NOT_FOUND, actual.getStatusCode());
@@ -156,7 +166,7 @@ public class EventControllerTest {
         } catch (IllegalAccessException ignored) {}
         toSave.addParticipant(new Participant());
         var invitationCode = toSave.getId();
-        repo.save(toSave);
+        eventRepo.save(toSave);
         var actual = sut.getParticipantsByInvitationCode(invitationCode);
         assertEquals(OK, actual.getStatusCode());
     }
@@ -169,7 +179,7 @@ public class EventControllerTest {
         } catch (IllegalAccessException ignored) {}
         toSave.addParticipant(new Participant());
         var invitationCode = toSave.getId();
-        repo.save(toSave);
+        eventRepo.save(toSave);
         var actual = sut.getParticipantsByInvitationCode(invitationCode);
         assertNotNull(actual.getBody());
     }
@@ -182,7 +192,7 @@ public class EventControllerTest {
         } catch (IllegalAccessException ignored) {}
         toSave.addParticipant(new Participant());
         var invitationCode = toSave.getId();
-        repo.save(toSave);
+        eventRepo.save(toSave);
         var actual = sut.getParticipantsByInvitationCode(invitationCode);
         assertNotNull(actual.getBody().get(0));
     }
@@ -192,6 +202,122 @@ public class EventControllerTest {
         var actual = sut.getParticipantsByInvitationCode(null);
         assertEquals(BAD_REQUEST, actual.getStatusCode());
     }
+
+    @Test
+    void checkUpdateEvent() {
+        Event event = new Event("event");
+
+        try {
+            setId(event, UUID.randomUUID());
+        } catch (IllegalAccessException ignored) {}
+
+        eventRepo.save(event);
+
+        event.setTitle("foo");
+
+        sut.updateEvent(principal, event);
+
+        verify(messagingTemplate).convertAndSend("/topic/"+event.getId(), event);
+        verify(messagingTemplate).convertAndSendToUser(eq(principal.getName()), eq("queue/reply"),
+                eq(StatusEntity.ok("event:update "+event.getId())));
+
+        assertTrue(eventRepo.existsById(event.getId()));
+        var repoEvent = eventRepo.getReferenceById(event.getId());
+        assertEquals("foo", repoEvent.getTitle());
+    }
+
+    @Test
+    void checkUpdateEventNull() {
+        Event event = new Event("event");
+
+        try {
+            setId(event, UUID.randomUUID());
+        } catch (IllegalAccessException ignored) {}
+
+        eventRepo.save(event);
+
+        event.setTitle(null);
+
+        sut.updateEvent(principal, event);
+
+        verify(messagingTemplate).convertAndSendToUser(eq(principal.getName()),eq("/queue/reply"),
+                eq(StatusEntity.badRequest("Title should not be empty")));
+    }
+
+    @Test
+    void checkUpdateEventWrongClass() {
+        Participant participant = new Participant();
+
+        sut.updateEvent(principal, participant);
+
+        verify(messagingTemplate).convertAndSendToUser(eq(principal.getName()) ,eq("/queue/reply"),
+                eq(StatusEntity.badRequest("Payload should be an Event", true)));
+
+        assertFalse(eventRepo.existsById(participant.getId()));
+    }
+
+    @Test
+    void checkUpdateEventNotFound() {
+        Event event = new Event("event");
+
+        try {
+            setId(event, UUID.randomUUID());
+        } catch (IllegalAccessException ignored) {}
+
+        sut.updateEvent(principal, event);
+        verify(messagingTemplate).convertAndSendToUser(eq(principal.getName()),eq("/queue/reply"),
+                eq(StatusEntity.notFound("Event not found",true)));
+
+        assertFalse(eventRepo.existsById(event.getId()));
+    }
+
+    //TODO: Incorporate admin password into tests for deleteEvent()
+    @Test
+    void checkDeleteEvent() {
+        Event event = new Event("event");
+
+        try {
+            setId(event, UUID.randomUUID());
+        } catch (IllegalAccessException ignored) {}
+
+        eventRepo.save(event);
+
+        assertTrue(eventRepo.existsById(event.getId()));
+
+        sut.deleteEvent(principal, event);
+
+        verify(messagingTemplate).convertAndSend("/topic/"+event.getId(), event);
+        verify(messagingTemplate).convertAndSendToUser(eq(principal.getName()), eq("queue/reply"),
+                eq(StatusEntity.ok("event:delete "+event.getId())));
+        assertFalse(eventRepo.existsById(event.getId()));
+    }
+
+    @Test
+    void checkDeleteEventWrongClass() {
+        Participant participant = new Participant();
+
+        sut.deleteEvent(principal, participant);
+
+        verify(messagingTemplate).convertAndSendToUser(eq(principal.getName()) ,eq("/queue/reply"),
+                eq(StatusEntity.badRequest("Payload should be an Event",true)));
+    }
+
+    @Test
+    void checkDeleteEventNotFound() {
+        Event event = new Event("foo");
+
+        try {
+            setId(event, UUID.randomUUID());
+        } catch (IllegalAccessException ignored) {}
+
+        sut.deleteEvent(principal, event);
+
+        verify(messagingTemplate).convertAndSendToUser(eq(principal.getName()),eq("/queue/reply"),
+                eq(StatusEntity.notFound("Event not found",true)));
+        assertFalse(eventRepo.existsById(event.getId()));
+    }
+
+    //TODO: add a test for unauthorized access
 }
 
 
