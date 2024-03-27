d30e339576573a06be91498dd1674e4c85c7fdd7
David Potskhishvili <D.Potskhishvili@student.tudelft.nl>
2024-03-27 20:58:52 +0100

    Prepared template

---
+++ client/src/main/java/client/scenes/AdminPanelCtrl.java
@@ -0,0 +1,10 @@
+package client.scenes;
+
+import commons.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class AdminPanelCtrl {
+
+}
