9d338d6376d225f3e3f433619d184ff538e1b325
Marios Kailis <m.kailis@student.tudelft.nl>
2024-04-09 17:03:04 +0200

    Create Update Involved frame handler

---
+++ client/src/main/java/client/utils/frameHandlers/UpdateInvolvedHandler.java
@@ -0,0 +1,34 @@
+package client.utils.frameHandlers;
+
+import client.utils.EventDataHandler;
+import commons.Involved;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+
+import java.lang.reflect.Type;
+
+/**
+ * Frame handler for /involved:update topic
+ */
+public class UpdateInvolvedHandler implements StompFrameHandler {
+    private final EventDataHandler dataHandler;
+
+    /**
+     * Constructor for the UpdateInvolvedHandler
+     *
+     * @param dataHandler reference to the dataHandler
+     */
+    public UpdateInvolvedHandler(EventDataHandler dataHandler) {
+        this.dataHandler = dataHandler;
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return Involved.class;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+        dataHandler.getUpdateInvolved((Involved) payload);
+    }
+}
