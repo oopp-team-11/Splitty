fd94aef4d3bd14e699eafa544f8edce29d0d234f
Jakub Pietrzak <jkpietrzak@student.tudelft.nl>
2024-04-14 01:12:55 +0200

    Cleaned up the server side

--- server/src/test/java/server/api/TestParticipantRepository.java
+++ server/src/test/java/server/api/TestParticipantRepository.java
@@ -40,61 +40,51 @@ public class TestParticipantRepository implements ParticipantRepository {
 
     @Override
     public List<Participant> findAll(Sort sort) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public List<Participant> findAllById(Iterable<UUID> ids) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public <S extends Participant> List<S> saveAll(Iterable<S> entities) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public void flush() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public <S extends Participant> S saveAndFlush(S entity) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public <S extends Participant> List<S> saveAllAndFlush(Iterable<S> entities) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public void deleteAllInBatch(Iterable<Participant> entities) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void deleteAllByIdInBatch(Iterable<UUID> ids) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void deleteAllInBatch() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public Participant getOne(UUID id) {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -116,19 +106,16 @@ public class TestParticipantRepository implements ParticipantRepository {
 
     @Override
     public <S extends Participant> List<S> findAll(Example<S> example) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public <S extends Participant> List<S> findAll(Example<S> example, Sort sort) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Page<Participant> findAll(Pageable pageable) {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -148,7 +135,6 @@ public class TestParticipantRepository implements ParticipantRepository {
 
     @Override
     public Optional<Participant> findById(UUID id) {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -165,7 +151,6 @@ public class TestParticipantRepository implements ParticipantRepository {
 
     @Override
     public void deleteById(UUID id) {
-        // TODO Auto-generated method stub
 
     }
 
@@ -176,49 +161,41 @@ public class TestParticipantRepository implements ParticipantRepository {
 
     @Override
     public void deleteAllById(Iterable<? extends UUID> ids) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void deleteAll(Iterable<? extends Participant> entities) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void deleteAll() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public <S extends Participant> Optional<S> findOne(Example<S> example) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public <S extends Participant> Page<S> findAll(Example<S> example, Pageable pageable) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public <S extends Participant> long count(Example<S> example) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public <S extends Participant> boolean exists(Example<S> example) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public <S extends Participant, R> R findBy(Example<S> example, Function<FetchableFluentQuery<S>, R> queryFunction) {
-        // TODO Auto-generated method stub
         return null;
     }
 }
